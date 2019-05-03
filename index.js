3756f56d428326286df1a000c086e9acc8c975de
nayamarosa <falecom@nayama.com.br>
2019-05-03 14:07:03 -0300

    funções para calculo e checagem de digitor validador ok. função cardValidator ok. testes da função cardValidator passando.

--- index.js
+++ index.js
@@ -1,98 +1,79 @@
-// function cardValidator (cardnumber) {
-
-// }
-
-function checkLength (cardnumber) {
-  
-  return cardnumber.toString().length === 16;
-  
+function cardValidator(cardnumber) {
+  if (checkLength(cardnumber) === false) {
+    return false;
+  } else if (checkIfSameNumber(cardnumber) === false) {
+    return false;
+  } else {
+    return checkCardDigitValidator(cardnumber);
+  }
 }
-
-function checkIfSameNumber (cardnumber) {
-  
+function checkLength(cardnumber) {
+  return cardnumber.toString().length === 16;
+}
+function checkIfSameNumber(cardnumber) {
   const card = cardnumber.substring(0, 15).split('');
   const checkRepeatNumber = card.every(num => num === card[0]);
   if (checkRepeatNumber === true) {
-
     return false;
-  
   } else {
-
     return true;
-  
   }
-
 }
-
-function oddPositionDigitsCalculate (cardnumber) {
-  
+function oddPositionDigitsCalculate(cardnumber) {
   const fifteenDigits = cardnumber.substring(0, 15).split('');
-
   const transformToNumbers = fifteenDigits.map(Number);
-
   let arrayOddsToSum = [];
-
   for (num in transformToNumbers) {
     let oddsNumPosition = num % 2 === 0;
-
-    if (oddsNumPosition){
-      
+    if (oddsNumPosition) {
       let multiplyNumber = transformToNumbers[num] * 2;
-
-      if(multiplyNumber > 9){
+      if (multiplyNumber > 9) {
         multiplyNumber = multiplyNumber - 9;
       }
-      arrayOddsToSum.push(multiplyNumber)
+      arrayOddsToSum.push(multiplyNumber);
     }
   }
-  
-  let allOddsDigitsSum = arrayOddsToSum.reduce(function (a, b) {
-    
+  let allOddsDigitsSum = arrayOddsToSum.reduce(function(a, b) {
     return a + b;
-    
   });
-
   return allOddsDigitsSum;
-  
 }
-
-function evenPositionDigitsCalculate (cardnumber) {
-  
+function evenPositionDigitsCalculate(cardnumber) {
   const fifteenDigits = cardnumber.substring(0, 15).split('');
-
   const transformToNumbers = fifteenDigits.map(Number);
-  
   let arrayEvensToSum = [];
-
   for (num in transformToNumbers) {
     let evensNumPosition = num % 2 !== 0;
-
-    if (evensNumPosition){
-      
+    if (evensNumPosition) {
       let multiplyNumber = transformToNumbers[num];
-      arrayEvensToSum.push(multiplyNumber)
+      arrayEvensToSum.push(multiplyNumber);
     }
   }
-  
-  let allEvensDigitsSum = arrayEvensToSum.reduce(function (a, b) {
-    
+  let allEvensDigitsSum = arrayEvensToSum.reduce(function(a, b) {
     return a + b;
-    
   });
-
   return allEvensDigitsSum;
-  
 }
-
-function sumAllDigits(cardnumber){
+function sumAllDigits(cardnumber) {
   return oddPositionDigitsCalculate(cardnumber) + evenPositionDigitsCalculate(cardnumber);
 }
-
-checkLength('4539983237489397');
-checkIfSameNumber('4539983237489397');
-sumAllDigits('4539983237489397')
-
-// module.exports.cardValidator = cardValidator;
-
+function validateCardLastDigitCalculate(cardnumber) {
+  const sumResult = sumAllDigits(cardnumber);
+  const floatNumber = sumResult / 10;
+  const roundNumber = Math.ceil(floatNumber) * 10;
+  const differenceToValidate = roundNumber - sumResult;
+  return differenceToValidate;
+}
+function checkCardDigitValidator(cardnumber) {
+  const checkLastDigitValidate = cardnumber.substring(15, 16).split('');
+  const transformToNumbers = checkLastDigitValidate.map(Number);
+  if (validateCardLastDigitCalculate(cardnumber) === transformToNumbers[0]) {
+    return true;
+  } else {
+    return false;
+  }
+}
+cardValidator('4539983237489397');
+module.exports.cardValidator = cardValidator;
 module.exports.checkLength = checkLength;
 module.exports.checkIfSameNumber = checkIfSameNumber;
--- test/card-validator.spec.js
+++ test/card-validator.spec.js
@@ -5,23 +5,26 @@ let cardValidator = require("../index");
 
 describe("card lib", () => {
 
-    // describe("cardValidator", () => {
-    //     it("card length is valid to check", () => {
-    //         expect(cardValidator.cardValidator("57097570806")).to.equal(true);
-    //     });
-    //     it("card length is valid to check", () => {
-    //         expect(cardValidator.cardValidator("145872")).to.equal(false);
-    //     });
-    //     it("card different numbers is valid to check", () => {
-    //         expect(cardValidator.cardValidator("11111111111")).to.equal(false);
-    //     });
-    //     it("card codes validate is valid", () => {
-    //         expect(cardValidator.cardValidator("57097570806")).to.equal(true);
-    //     });
-    //     it("card codes validate is valid", () => {
-    //         expect(cardValidator.cardValidator("75489632145")).to.equal(false);
-    //     });
-    // });
+    describe("cardValidator", () => {
+        it("card length is valid to check", () => {
+            expect(cardValidator.cardValidator("4539983237489397")).to.equal(true);
+        });
+        it("card length is valid to check", () => {
+            expect(cardValidator.cardValidator("145872")).to.equal(false);
+        });
+        it("card length is valid to check", () => {
+            expect(cardValidator.cardValidator("14514587965412358872")).to.equal(false);
+        });
+        it("card different numbers is valid to check", () => {
+            expect(cardValidator.cardValidator("1111111111111111")).to.equal(false);
+        });
+        it("card codes validate is valid", () => {
+            expect(cardValidator.cardValidator("4539983237489397")).to.equal(true);
+        });
+        it("card codes validate is valid", () => {
+            expect(cardValidator.cardValidator("4539983237489395")).to.equal(false);
+        });
+    });
 
     describe("checkLength", () => {
         it("it has 16 numbers", () => {
@@ -43,17 +46,4 @@ describe("card lib", () => {
             expect(cardValidator.checkIfSameNumber("2543875466851247")).to.equal(true);
         });
     });
-
-    // describe("checkCodesValidate", () => {
-    //     it("the first number is equal to module", () => {
-    //         expect(cardValidator.checkCodesValidate("57097570806")).to.equal(true);
-    //     });
-    //     it("the first number is equal to module", () => {
-    //         expect(cardValidator.checkCodesValidate("73516511834")).to.equal(true);
-    //     });
-    //     it("the first number is equal to module", () => {
-    //         expect(cardValidator.checkCodesValidate("75489632145")).to.equal(false);
-    //     });
-    // });
-    
 });
